2ed60081ecaf8ef6278c6ba99a2f4c4208e31478
Maple <crazy.maple@foxmail.com>
2016-05-27 13:00:11 +0800

    commit

---
+++ service/FansService.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maple
+ * Date: 16/5/27
+ * Time: 10:06
+ */
+
+namespace app\service;
+
+use Yii;
+use app\service\BaseService;
+use app\models\Fans;
+
+class FansService extends BaseService
+{
+
+}
