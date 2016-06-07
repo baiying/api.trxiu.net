d19e91bfb101ddd6a259c2c20275df99410867c6
Maple <crazy.maple@foxmail.com>
2016-06-07 15:32:28 +0800

    修改添加修改粉丝参数

--- controllers/FansController.php
+++ controllers/FansController.php
@@ -28,14 +28,33 @@ class FansController extends BaseController
         $rule = [
             'wx_openid' => ['type' => 'string', 'required' => TRUE],
             'wx_name' => ['type' => 'string', 'required' => TRUE],
+            'wx_sex' => ['type' => 'int', 'required' => FALSE],
             'wx_thumb' => ['type' => 'string', 'required' => TRUE],
+            'anchor_id' => ['type' => 'int', 'required' => FALSE],
+            'wx_access_token' => ['type' => 'string', 'required' => FALSE],
+            'wx_refresh_token' => ['type' => 'string', 'required' => FALSE],
+            'wx_access_token_expire' => ['type' => 'int', 'required' => FALSE],
+            'wx_continue' => ['type' => 'string', 'required' => FALSE],
+            'wx_province' => ['type' => 'string', 'required' => FALSE],
+            'wx_city' => ['type' => 'string', 'required' => FALSE],
+            'wx_unionid' => ['type' => 'string', 'required' => FALSE],
         ];
         $args = $this->getRequestData($rule, Yii::$app->request->get());
 
         //构建查询条件
-        $data['wx_openid'] = $args['wx_openid'];
-        $data['wx_name'] = $args['wx_name'];
-        $data['wx_thumb'] = $args['wx_thumb'];
+        $data['wx_openid'] = $args['wx_openid'];//微信账号openid
+        $data['wx_name'] = $args['wx_name'];//微信账号名称
+        $data['wx_thumb'] = $args['wx_thumb'];//微信头像
+        $data['wx_sex'] = isset($args['wx_sex']) ? $args['wx_sex'] : 3;//性别,1男，2女，3未知
+        $data['anchor_id'] = isset($args['wx_sex']) ? $args['anchor_id'] : 0;//主播ID，主播用户该字段大于0，非主播用户为0
+        isset($args['wx_access_token']) && $data['wx_access_token'] = $args['wx_access_token'];//微信账号access_token，有效期2小时
+        isset($args['wx_refresh_token']) && $data['wx_refresh_token'] = $args['wx_refresh_token'];//微信账号refresh_token，有效期30天，到期后需要用户重新授权
+        isset($args['wx_access_token_expire']) && $data['wx_access_token_expire'] = $args['wx_access_token_expire'];//access_token过期时间戳
+        isset($args['wx_continue']) && $data['wx_continue'] = $args['wx_continue'];//国家
+        isset($args['wx_province']) && $data['wx_province'] = $args['wx_province'];//省份
+        isset($args['wx_city']) && $data['wx_city'] = $args['wx_city'];//城市
+        isset($args['wx_unionid']) && $data['wx_unionid '] = $args['wx_unionid'];//微信账号unionid
+        $data['create_time'] = time();
         $this->fansService = new FansService();
         $result = $this->fansService->addFans($data);
         if($result['status']==false){
@@ -117,19 +136,37 @@ class FansController extends BaseController
 
         $this->checkMethod('get');
         $rule = [
-            'fans_id' => ['type' => 'string', 'required' => TRUE, 'default' => ''],
+            'fans_id' => ['type' => 'string', 'required' => TRUE],
             'wx_openid' => ['type' => 'string', 'required' => FALSE],
             'wx_name' => ['type' => 'string', 'required' => FALSE],
+            'wx_sex' => ['type' => 'int', 'required' => FALSE],
             'wx_thumb' => ['type' => 'string', 'required' => FALSE],
+            'anchor_id' => ['type' => 'int', 'required' => FALSE],
+            'wx_access_token' => ['type' => 'string', 'required' => FALSE],
+            'wx_refresh_token' => ['type' => 'string', 'required' => FALSE],
+            'wx_access_token_expire' => ['type' => 'int', 'required' => FALSE],
+            'wx_continue' => ['type' => 'string', 'required' => FALSE],
+            'wx_province' => ['type' => 'string', 'required' => FALSE],
+            'wx_city' => ['type' => 'string', 'required' => FALSE],
+            'wx_unionid' => ['type' => 'string', 'required' => FALSE],
         ];
         $args = $this->getRequestData($rule, Yii::$app->request->get());
 
         //构建查询条件
         $where['fans_id'] = $args['fans_id'];
-        isset($args['wx_openid']) && $data['wx_openid'] = $args['wx_openid'];
-        isset($args['wx_name']) && $data['wx_name'] = $args['wx_name'];
-        isset($args['wx_thumb']) && $data['wx_thumb'] = $args['wx_thumb'];
-
+        isset($args['wx_openid']) && $data['wx_openid'] = $args['wx_openid'];//微信账号openid
+        isset($args['wx_name']) && $data['wx_name'] = $args['wx_name'];//微信账号名称
+        isset($args['wx_thumb']) && $data['wx_thumb'] = $args['wx_thumb'];//微信头像
+        isset($args['wx_sex']) && $data['wx_sex'] =  $args['wx_sex'];//性别,1男，2女，3未知
+        isset($args['wx_sex']) &&$data['anchor_id'] =  $args['anchor_id'];//主播ID，主播用户该字段大于0，非主播用户为0
+        isset($args['wx_access_token']) && $data['wx_access_token'] = $args['wx_access_token'];//微信账号access_token，有效期2小时
+        isset($args['wx_refresh_token']) && $data['wx_refresh_token'] = $args['wx_refresh_token'];//微信账号refresh_token，有效期30天，到期后需要用户重新授权
+        isset($args['wx_access_token_expire']) && $data['wx_access_token_expire'] = $args['wx_access_token_expire'];//access_token过期时间戳
+        isset($args['wx_continue']) && $data['wx_continue'] = $args['wx_continue'];//国家
+        isset($args['wx_province']) && $data['wx_province'] = $args['wx_province'];//省份
+        isset($args['wx_city']) && $data['wx_city'] = $args['wx_city'];//城市
+        isset($args['wx_unionid']) && $data['wx_unionid '] = $args['wx_unionid'];//微信账号unionid
+        
         $this->fansService = new FansService();
         $result = $this->fansService->upFans($data,$where);
         if($result['status']==false){
