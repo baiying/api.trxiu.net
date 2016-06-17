5ec45a5077183b5e2b0cf835350df56d98e518bd
evan.bai <cn.baiying@gmail.com>
2016-06-17 17:36:49 +0800

    update

--- config/params.php
+++ config/params.php
@@ -5,8 +5,7 @@ return [
 	 * 调用接口的APP列表，key为各应用的APPID，value为各应用的秘钥
 	 */
     'appKeyValues' => [
-    	'www.e2047.com' => 'CEADAD02F52E41ECA86F1709355D3CA6DA7C',
-    	'admin.e2047.com' => 'A2C64E814A96B5188B938DCAE8089259DEB2',
-    	'admin.show.com' => 'A2C64E814A96B5188B938DCAE8089259DEB2',
+    	'wechat.trxiu.net' => 'CEADAD02F52E41ECA86F1709355D3CA6DA7C',
+    	'admin.trxiu.net' => 'A2C64E814A96B5188B938DCAE8089259DEB2',
     ],
 ];
